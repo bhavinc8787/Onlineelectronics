ce8a78587b4829bbf9b05a32297c7ed66f71ad7c
bhavinc8787 <89356256+bhavinc8787@users.noreply.github.com>
2024-08-12 17:24:30 +0530

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,25 +1,22 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const images = [
-  'https://img.freepik.com/free-photo/open-laptop_144627-12148.jpg?t=st=1723224208~exp=1723227808~hmac=06601723253ded87f049086c818e0d4da13c304e5fb7ab27ad92ebe4c4973727&w=900',
-  'https://img.freepik.com/premium-photo/magnificent-air-conditioner-isolated-white-background_787273-72998.jpg?w=740'
-   
+  'https://img.freepik.com/free-psd/isolated-modern-laptop_23-2150629066.jpg?t=st=1723462556~exp=1723466156~hmac=b98bc326b3785e4016ffccee31bb9832185fe374cda49bbc0aa7cf5b89cfafa3&w=740',
+  'https://img.freepik.com/free-vector/refrigerator-with-lots-food_1308-105555.jpg?t=st=1723462726~exp=1723466326~hmac=01fba9eb6569065e4964df7642fa07d5b951aa1fc4b844b585522d8ad12c5535&w=740'
 ];
 
 function Banner() {
-  const carouselRef = useRef(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const intervalRef = useRef(null);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (carouselRef.current) {
-        carouselRef.current.scrollBy({
-          left: carouselRef.current.offsetWidth,
-          behavior: 'smooth'
-        });
-      }
-    }, 3000); // Change images every 3 seconds
 
-    return () => clearInterval(interval);
+    intervalRef.current = setInterval(() => {
+      setCurrentIndex(prevIndex => (prevIndex + 1) % images.length);
+    }, 6000); 
+
+   
+    return () => clearInterval(intervalRef.current);
   }, []);
 
   return (
@@ -33,25 +30,24 @@ function Banner() {
           <p className="text-xl font-semibold">
             Explore our wide range of electronics and find the latest gadgets and technology at unbeatable prices. Shop now and stay ahead with the best in tech!
           </p>
-
-         
         </div>
-       
       </div>
-      <div className="w-full md:w-1/2 order-1 overflow-hidden">
+      <div className="w-full md:w-1/2 order-1 overflow-hidden relative">
         <div
-          className="flex"
-          ref={carouselRef}
+          className="flex transition-transform duration-1000 ease-in-out"
+          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
         >
           {images.map((src, index) => (
             <img
               key={index}
               src={src}
               alt={`Slide ${index + 1}`}
-              className="w-full h-auto flex-shrink-0"
+              className="w-full flex-shrink-0 object-cover"
+              style={{ minHeight: '300px' }} // Adjust the height as needed
             />
           ))}
         </div>
+        
       </div>
     </div>
   );
