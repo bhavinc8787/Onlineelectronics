441913b291f97ee04b4c6eb6de876c6bfeb9498f
bhavinc8787 <89356256+bhavinc8787@users.noreply.github.com>
2024-08-13 19:48:34 +0530

    Update CategoryPage.jsx

--- src/components/CategoryPage.jsx
+++ src/components/CategoryPage.jsx
@@ -19,11 +19,12 @@ const CategoryPage = () => {
         </span>
       </h1>
       
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-10">
-        {products.map(product => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
+      <div className="mt-10">
+  {products.map(product => (
+    <ProductCard key={product.id} product={product} />
+  ))}
+</div>
+
     </div>
   );
 };
